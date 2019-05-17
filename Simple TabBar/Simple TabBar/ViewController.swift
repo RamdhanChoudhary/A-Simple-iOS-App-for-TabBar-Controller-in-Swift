b2f0fba196ba099d7e0f22cdaf296d3c9f761a3a
RDC <ramdhan.cse@gmail.com>
2019-05-17 14:07:26 +0530

    Initial commit with fresh code

---
+++ Simple TabBar/Simple TabBar/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Simple TabBar
+//
+//  Created by RAMDHAN CHOUDHARY on 16/05/19.
+//  Copyright © 2019 RDC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController  {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        //TODO : Everything you need to do is in storyboard, Select default View controller, Editor Menu -> Embded In -> TabBar Controller -> Add another two View controllers -> Connect Segue from TabBar controller to View controllers -> Give name and Icon image to TabBar buttons. You are done!!
+    }
+
+
+}
+
